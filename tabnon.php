d4b95a8058e6a61968b78b04e0e5cb5da368d774
CHEVALIER Tony <tony@tony974.net>
2013-01-24 15:02:40 +0400

    addition des pages fans/non-fans

---
+++ tabnon.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>TS</title>
+<meta name="ROBOTS" content="NOINDEX,NOFOLLOW">
+<style>
+ body {
+             	overflow:hidden;
+				width:520px;
+				margin:0;
+				padding:0;
+				border:0;
+				height:2020px;
+            }
+</style>
+</head>
+
+<body>
+
+<div class="content">
+INSEREZ LE CONTENU ICI POUR LES NON FANS
+</div>
+
+</body>
+</html>
---
+++ taboui.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>TS</title>
+<meta name="ROBOTS" content="NOINDEX,NOFOLLOW">
+<style>
+ body {
+             	overflow:hidden;
+				width:520px;
+				margin:0;
+				padding:0;
+				border:0;
+           }
+</style>
+</head>
+
+<body>
+
+<div class="content">
+INSEREZ ICI LE CONTENU POUR LES FANS
+
+</div>
+
+</body>
+</html>
